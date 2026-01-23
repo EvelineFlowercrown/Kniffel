6b26e035c3b62f6fbdcb1da275f3fee96aff0efa
Eveline <eveflowercrown@gmail.com>
2026-01-23 12:02:29 +0100

    redid main.py

--- main.py
+++ main.py
@@ -17,16 +17,37 @@ def Start_DB():
     return db
 
 
-def login_players():
+def login_player(db):
+    username = input("Bitte gib deinen Username ein")
+    saved_password = db.select_where(
+        table_name="player_account",
+        conditions={"username": username},
+        columns=["password"]
+    )
+    if saved_password is not None:
+        try_again = False
+        password = input("Bitte gib dein passwort ein")
+        if saved_password == password:
+            return username
+        else:
+            print("Wrong Password")
+            input("Willst du es noch mal versuchen")
+
+
+
+def login_players(db):
     logged_in_players = []
+    for i in range(h.get_integer_input_upto("Wie viele ",5)):
+        logged_in_players.append(login_player(db))
 
     return logged_in_players
 
 
 def main():
     db = Start_DB()
-    players = login_players()
+    players = login_players(db)
 
 
-if __name__ == "main":
+if __name__ == "__main__":
+    print("hi")
     main()
